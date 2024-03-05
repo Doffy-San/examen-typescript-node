a51645800ae90006eec6153828d6ef706d2196a6
Skampo-San <72740149+Nino-Saadi@users.noreply.github.com>
2024-03-05 16:27:00 +0100

    Exercice 3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { readFileSync } from "fs";
 
-function getStatistics(): number{
-  const persons: number = JSON.parse(readFileSync("./persons.json").toString());
-  return persons;
+function getStatistics(): number {
+  const persons: any[] = JSON.parse(readFileSync("./persons.json").toString());
+  let maxAge = 0;
+  for (const person of persons) {
+    if (person.age > maxAge) {
+      maxAge = person.age;
+    }
+  }
+  return maxAge;
 }
+
+const ageMax = getStatistics();
+console.log("L'âge maximal est de", ageMax);
 
 function displayResult() {
   console.log(getStatistics());
